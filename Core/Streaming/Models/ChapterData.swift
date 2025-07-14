562acd7bd59a9d4ce1d726617257ce34cf3fb164
Mehdi <mehdi@neocore.tech>
2025-07-14 08:10:26 +0330

    Update ChapterData.swift

--- Core/Streaming/Models/ChapterData.swift
+++ Core/Streaming/Models/ChapterData.swift
@@ -18,31 +18,8 @@ struct ChapterDataModel {
 }
 
 
-struct ChapterData: Equatable, Codable {
-    let chapterNumber: Int
-    let title: String
-    let content: String
-    let status: String
-    let complete: Bool
-    
-    enum CodingKeys: String, CodingKey {
-        case chapterNumber = "chapter_number"
-        case title
-        case content
-        case status
-        case complete
-    }
-}
-
 // MARK: - NEW CODE
 
-/// A single link in the subtitles .m3u8, e.g. (vttURL, duration).
-struct SubtitleSegmentLink {
-    let urlString: String
-    let duration: Double
-    var minStart: Double
-    var maxEnd: Double 
-}
 
 /// Each word in a single .vtt file.
 struct WordTimestamp: Identifiable {
