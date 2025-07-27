7fe2b76f530279415e475d789c07b20074804cb3
Mehdi <mehdi@neocore.tech>
2025-07-27 11:13:40 +0330

    Delete AppStateManager.swift

--- App/Shared/AppStateManager.swift
+++
@@ -1,9 +0,0 @@
-import SwiftUI
-
-class AppStateManager: ObservableObject {
-    @Published var hasShownHomeIntro = false
-    
-    static let shared = AppStateManager()
-    
-    private init() {}
-}
