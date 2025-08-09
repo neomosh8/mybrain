2e491e8bf0971f8168c02ebdc8a9d73928c66484
Mehdi <mehdi@neocore.tech>
2025-08-09 12:11:36 +0330

    Update GlobalServices.swift

--- Common/Utilities/GlobalServices.swift
+++ Common/Utilities/GlobalServices.swift
@@ -1,3 +1,6 @@
 import Foundation
 
 let networkService = NetworkServiceManager.shared
+let bluetoothService = BTService.shared
+let feedbackService = FeedbackService.shared
+let feedbackBuffer: FeedbackBufferManager = .init()
