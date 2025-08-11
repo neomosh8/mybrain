349ea2589fcc55bc2c6ef111ba54fa103c409cd6
Mehdi <mehdi@neocore.tech>
2025-08-11 08:23:53 +0330

    Update AnimatedSubtitleView.swift

--- Common/Components/Misc/AnimatedSubtitleView.swift
+++ Common/Components/Misc/AnimatedSubtitleView.swift
@@ -3,27 +3,14 @@ import NaturalLanguage
 
 struct AnimatedSubtitleView: View {
     @ObservedObject var listeningViewModel: ListeningViewModel
-    
     let thoughtId: String
     let chapterNumber: Int
-    
-    @State private var paragraphs: [[WordData]] = []
-    
-    init(
-        listeningViewModel: ListeningViewModel,
-        thoughtId: String,
-        chapterNumber: Int,
-    ) {
-        self.listeningViewModel = listeningViewModel
-        self.thoughtId = thoughtId
-        self.chapterNumber = chapterNumber
-    }
     
     var body: some View {
         ScrollViewReader { proxy in
             ScrollView {
                 AnimatedWordsView(
-                    paragraphs: paragraphs,
+                    paragraphs: listeningViewModel.paragraphs,
                     currentWordIndex: listeningViewModel.currentWordIndex,
                     showOverlay: listeningViewModel.currentWordIndex >= 0,
                     wordFont: .body,
@@ -39,86 +26,13 @@ struct AnimatedSubtitleView: View {
                         proxy.scrollTo(newIndex, anchor: .center)
                     }
                 }
-                
-                if newIndex >= 0 && newIndex < listeningViewModel.allWords.count {
-                    let word = listeningViewModel.allWords[newIndex].text
-                    sendFeedback(word: word, thoughtId: thoughtId, chapterNumber: chapterNumber)
-                }
             }
             .onChange(of: listeningViewModel.allWords) { _, _ in
-                buildParagraphs()
+                listeningViewModel.buildParagraphs()
             }
             .onAppear {
-                buildParagraphs()
+                listeningViewModel.buildParagraphs()
             }
         }
-    }
-    
-    private func buildParagraphs() {
-        let allText = listeningViewModel.allWords.map { $0.text }.joined(separator: " ")
-        let tagger = NLTagger(tagSchemes: [.nameType, .lexicalClass])
-        tagger.string = allText
-        
-        let commonUppercaseWords: Set<String> = ["I", "I'm", "I'll", "I've", "I'd", "Dr", "Mr", "Mrs", "Ms"]
-        
-        var currentParagraph: [WordData] = []
-        var newParagraphs: [[WordData]] = []
-        var textIndex = allText.startIndex
-        
-        for (index, wordTimestamp) in listeningViewModel.allWords.enumerated() {
-            let wordData = WordData(
-                originalIndex: index,
-                text: wordTimestamp.text,
-                startTime: wordTimestamp.start,
-                endTime: wordTimestamp.end
-            )
-            
-            let word = wordTimestamp.text
-            let firstChar = word.first
-            let isUppercase = firstChar?.isUppercase == true
-            
-            if let wordRange = allText.range(of: word, range: textIndex..<allText.endIndex) {
-                var shouldStartNewParagraph = false
-                
-                if isUppercase && !currentParagraph.isEmpty {
-                    if commonUppercaseWords.contains(word) {
-                        shouldStartNewParagraph = false
-                    } else {
-                        tagger.enumerateTags(in: wordRange, unit: .word, scheme: .nameType) { tag, _ in
-                            if tag == .personalName || tag == .placeName || tag == .organizationName {
-                                shouldStartNewParagraph = false
-                            } else {
-                                shouldStartNewParagraph = true
-                            }
-                            return false
-                        }
-                        
-                        if shouldStartNewParagraph {
-                            tagger.enumerateTags(in: wordRange, unit: .word, scheme: .lexicalClass) { tag, _ in
-                                if tag == .noun && word.count > 3 {
-                                    shouldStartNewParagraph = false
-                                }
-                                return false
-                            }
-                        }
-                    }
-                }
-                
-                if shouldStartNewParagraph {
-                    newParagraphs.append(currentParagraph)
-                    currentParagraph = []
-                }
-                
-                textIndex = wordRange.upperBound
-            }
-            
-            currentParagraph.append(wordData)
-        }
-        
-        if !currentParagraph.isEmpty {
-            newParagraphs.append(currentParagraph)
-        }
-        
-        paragraphs = newParagraphs
     }
 }
