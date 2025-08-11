4ec01a95dd315da3e5bdd8934136dca30e3747d2
Mehdi <mehdi@neocore.tech>
2025-08-11 08:54:24 +0330

    Update AnimatedSubtitleView.swift

--- Common/Components/Misc/AnimatedSubtitleView.swift
+++ Common/Components/Misc/AnimatedSubtitleView.swift
@@ -3,9 +3,6 @@ import SwiftUI
 struct AnimatedSubtitleView: View {
     @Binding var paragraphs: [[WordData]]
     @Binding var currentWordIndex: Int
-    var spacing: CGFloat = 4
-    var lineSpacing: CGFloat = 6
-    var bottomPadding: CGFloat = 70
     
     var body: some View {
         ScrollViewReader { proxy in
@@ -13,11 +10,7 @@ struct AnimatedSubtitleView: View {
                 AnimatedWordsView(
                     paragraphs: paragraphs,
                     currentWordIndex: currentWordIndex,
-                    showOverlay: currentWordIndex >= 0,
-                    wordFont: .body,
-                    spacing: spacing,
-                    lineSpacing: lineSpacing,
-                    bottomPadding: bottomPadding
+                    showOverlay: currentWordIndex >= 0
                 )
                 .padding()
             }
