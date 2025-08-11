10736f71382e50a7fb2a7cc005b84ab56ada1bac
Mehdi <mehdi@neocore.tech>
2025-08-11 08:54:22 +0330

    Update AnimatedParagraphView.swift

--- Common/Components/Misc/AnimatedParagraphView.swift
+++ Common/Components/Misc/AnimatedParagraphView.swift
@@ -3,9 +3,6 @@ import SwiftUI
 struct AnimatedParagraphView: View {
     @Binding var paragraphs: [[WordData]]
     @Binding var currentWordIndex: Int
-    var spacing: CGFloat = 4
-    var lineSpacing: CGFloat = 6
-    var bottomPadding: CGFloat = 70
 
     var body: some View {
         ScrollViewReader { proxy in
@@ -13,10 +10,7 @@ struct AnimatedParagraphView: View {
                 AnimatedWordsView(
                     paragraphs: paragraphs,
                     currentWordIndex: currentWordIndex,
-                    showOverlay: currentWordIndex >= 0,
-                    spacing: spacing,
-                    lineSpacing: lineSpacing,
-                    bottomPadding: bottomPadding
+                    showOverlay: currentWordIndex >= 0
                 )
                 .padding()
             }
